5ec48e9dd83da9a5a48c8bf2ebb134da68cbbc83
shubham-mhatre <mhatreshubham03@gmail.com>
2025-03-14 19:54:03 +0530

    creating angular components

---
+++ src/app/pages/new-leave/new-leave.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-leave',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-leave.component.html',
+  styleUrl: './new-leave.component.scss'
+})
+export class NewLeaveComponent {
+
+}

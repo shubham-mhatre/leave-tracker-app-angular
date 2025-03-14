88e06e0e0670bf62d0bb1b4077dbd91d48d396ab
shubham-mhatre <mhatreshubham03@gmail.com>
2025-03-14 23:06:14 +0530

    navbar implementation in layout component

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule, MatButtonModule,
+    RouterOutlet //used routerOutlet here as layout is parent component & dashboar is it's child
+    //after login we are redirecting to dashboard component while displaying navbar from layout component
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  loggedInUserData:any;
 
+  router = inject(Router);
+
+  constructor(){
+
+    //to display logged in email-id in navbar.
+    //retrieve user object data from localstorage & display it in html using interpolation
+    const userData=localStorage.getItem('user');
+    if(userData){
+      this.loggedInUserData=JSON.parse(userData);
+    }
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.router.navigateByUrl('');
+  }
 }
